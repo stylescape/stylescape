779ff15d8ddca9945c8cf7471b272d054351361c
Lars van Vianen <lars@vianen.com>
2023-08-23 17:36:28 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/scss/index.scss',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+                ],
+            },
+        ],
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin(
+            {
+                filename: 'index.css',
+            }
+        ),
+    ],
+
+};
