0ab6e5fb0b2401ed6528f2d12ac936c180a03a1e
Lars van Vianen <lars@vianen.com>
2023-08-27 09:00:12 +0200

    Update webpack.paths.ts

--- webpack.paths.ts
+++ webpack.paths.ts
@@ -1,5 +1,5 @@
-import path from 'path'
-import { fileURLToPath } from 'url';
+import path from "path"
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -7,13 +7,13 @@ const __dirname = path.dirname(__filename);
 const configPaths: any = {
 
   // Source files
-  src: path.resolve(__dirname, '/src'),
+  src: path.resolve(__dirname, "/src"),
 
   // Production build files
-  build: path.resolve(__dirname, '/dist'),
+  build: path.resolve(__dirname, "/dist"),
 
   // Static files that get copied to build folder
-  public: path.resolve(__dirname, '/public'),
+  public: path.resolve(__dirname, "/public"),
 
 };
 
