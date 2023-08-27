dc175c322f24ebca15527919361df6582df71d99
Lars van Vianen <lars@vianen.com>
2023-08-27 09:00:34 +0200

    Update webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,20 @@
 // webpack.config.ts
 
-import { merge } from 'webpack-merge';
-import configCommon from './webpack.common';
-import configDevelopment from './webpack.dev';
-import configProduction from './webpack.prod';
+import { merge } from "webpack-merge";
+import configCommon from "./webpack.common";
+import configDevelopment from "./webpack.dev";
+import configProduction from "./webpack.prod";
 
 
 // Config | Merge
 const config = (env: any, args: { mode: any; }) => {
     switch(args.mode) {
-        case 'development':
+        case "development":
             return merge(configCommon, configDevelopment);
-        case 'production':
+        case "production":
             return merge(configCommon, configProduction);
         default:
-            throw new Error('No matching configuration was found!');
+            throw new Error("No matching configuration was found!");
     }
 }
 
