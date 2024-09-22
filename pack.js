8b8a14322525d1079c2fc3ab634268ddccd22c2e
Lars van Vianen <lars@vianen.com>
2024-09-22 09:57:16 +0200

    Delete pack.js

--- pack.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    tasks: [
-        { name: 'Build', action: 'build', options: { minify: true } },
-        { name: 'Test', action: 'test' },
-        { name: 'Package', action: 'package', options: { format: 'zip' } },
-    ],
-};
