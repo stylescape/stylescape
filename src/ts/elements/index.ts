dbdb2f4ed40b3bea1125c345d865526b96869f93
Lars van Vianen <lars@vianen.com>
2025-05-12 08:44:35 +0200

    Clean ts

--- src/ts/elements/index.ts
+++
@@ -1,3 +0,0 @@
-export { BaseElementManager } from '../../engine/BaseElementManager'
-export { ExclusiveDetails } from './ExclusiveDetails'
-export { TableOfContentsBuilder } from './TableOfContentsBuilder'
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,4 @@
+import { GridManager } from '../../node_modules/unit.gl/ts/GridManager.js';
 
+
+new GridManager(); // auto-initializes
