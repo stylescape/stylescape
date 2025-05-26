5c000661684c5b8ce9fc0fdb9a649639d6b59455
Kerem <50572951+keremsoybakis@users.noreply.github.com>
2025-05-26 15:27:02 +0200

    new aside logic

--- src/ts/elements/AsideHandler.ts
+++ src/ts/elements/AsideHandler.ts
@@ -1,28 +1,47 @@
 export class AsideHandler {
-    private middleCenter: HTMLElement | null
+    private leftAside: HTMLElement | null
+    private rightAside: HTMLElement | null
     private leftSwitch: HTMLElement | null
     private rightSwitch: HTMLElement | null
+    private leftRibbon: HTMLElement | null
+    private rightRibbon: HTMLElement | null
 
     constructor() {
-        this.middleCenter = document.getElementById('middleCenter')
-        this.leftSwitch = document.getElementById('leftSwitch')
-        this.rightSwitch = document.getElementById('rightSwitch')
+        this.leftAside = document.getElementById("leftAside")
+        this.rightAside = document.getElementById("rightAside")
+        this.leftSwitch = document.getElementById("leftSwitch")
+        this.rightSwitch = document.getElementById("rightSwitch")
+        this.leftRibbon = document.getElementById("leftRibbon")
+        this.rightRibbon = document.getElementById("rightRibbon")
 
-        this.init()
+        this.activateIfNoRibbon()
+        this.setupSwitchListeners()
     }
 
-    private init(): void {
-        if (this.middleCenter && this.leftSwitch) {
-            this.leftSwitch.addEventListener('click', () => {
-                this.middleCenter!.classList.toggle('collapse-left')
-                this.leftSwitch!.classList.toggle('active')
+    private activateIfNoRibbon(): void {
+        if (this.leftAside && this.leftSwitch && !this.leftRibbon) {
+            this.leftAside.classList.add("active")
+            this.leftSwitch.classList.add("active")
+        }
+
+        if (this.rightAside && this.rightSwitch && !this.rightRibbon) {
+            this.rightAside.classList.add("active")
+            this.rightSwitch.classList.add("active")
+        }
+    }
+
+    private setupSwitchListeners(): void {
+        if (this.leftAside && this.leftSwitch) {
+            this.leftSwitch.addEventListener("click", () => {
+                this.leftAside!.classList.toggle("active")
+                this.leftSwitch!.classList.toggle("active")
             })
         }
 
-        if (this.middleCenter && this.rightSwitch) {
-            this.rightSwitch.addEventListener('click', () => {
-                this.middleCenter!.classList.toggle('collapse-right')
-                this.rightSwitch!.classList.toggle('active')
+        if (this.rightAside && this.rightSwitch) {
+            this.rightSwitch.addEventListener("click", () => {
+                this.rightAside!.classList.toggle("active")
+                this.rightSwitch!.classList.toggle("active")
             })
         }
     }
