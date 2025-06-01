289040c1f4f4ae2dd1f2361513ab2c8491f40a08
Lars van Vianen <lars@vianen.com>
2025-06-01 13:29:38 +0200

    Sidebar WIP

--- src/ts/index copy.ts
+++
@@ -1,8 +0,0 @@
-// import { GridManager } from 'unit.gl/ts//GridManager.js';
-
-// new GridManager(); // auto-initializes
-
-// // 👈 signals intentional side effect
-
-// // import 'unit.gl/grids.js';
-// // import { setupGridToggles } from 'unit.gl/grids.js';
